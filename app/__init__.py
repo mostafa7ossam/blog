7a28ff4aa1192b1b7c97e9592ea577c8807d1ebc
Mostafa <mostafa.h.zakaria@gmail.com>
2018-10-14 02:17:35 +0200

    remove flask moment

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from flask_moment import Moment
 import logging
 from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
@@ -14,7 +13,6 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-moment = Moment(app)
 login = LoginManager(app)
 login.login_view = 'login'
 #bootstrap = Bootstrap(app)
